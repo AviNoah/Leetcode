c7b8bef8868a47eacf34cb05903aa81285696fb2
Avi Noah <AviNoah2019@gmail.com>
2024-10-24 17:40:49 +0300

    added test

--- problems/prob_42.py
+++ problems/prob_42.py
@@ -1,79 +1,73 @@
 # https://leetcode.com/problems/trapping-rain-water
 
-"""
-Rain water can be collected only if its between two non-zero numbers,
-it ends being collected once the second number is bigger than the first.
-"""
+# def nge(heights: list[int]) -> tuple[list[int], int]:
+#     """
+#     Given a list of heights, return a list of indices, where for elem i,
+#     is the index to the nge
+
+#     nge is the next element that is greater or equal to the element at i
+
+#     -1 means no nge was found
+#     """
+#     results = [-1] * len(heights)  # By default none have nge
+
+#     index_stack: list[int] = list()
+#     area = 0
+
+#     # As long as it is descending, add to stack, when it is not
+#     # we will pop the stack until it is descending again.
+
+#     for i, h in enumerate(heights):
+#         # If desc
+#         if not index_stack or h < heights[index_stack[-1]]:
+#             index_stack.append(i)  # O(1)
+#             continue
+
+#         # If asc
+#         while index_stack and h >= heights[index_stack[-1]]:
+#             index = index_stack.pop()
+#             results[index] = i  # O(1)
+#             area += h - heights[index]
+
+#         index_stack.append(i)  # O(1)
+
+#     return results, area
 
 
-def nge(heights: list[int]) -> tuple[list[int], int]:
-    """
-    Given a list of heights, return a list of indices, where for elem i,
-    is the index to the nge
+# def sol(heights: list[int]) -> int:
+#     # We can implement a nge - next greatest or equal element, method, that will find the index of the
+#     # next biggest element for every index i
 
-    nge is the next element that is greater or equal to the element at i
+#     # Then, to simplify rain collection - for example between two big numbers we may have
+#     # bars that take the place of a rain water slot, we can deduce that the maximum
+#     # rain area between them is at most the distance between them, times the height of the first number
+#     # which is always smaller, we then go along the indices between them and deduct the values
+#     # of the bars since that is how much space they take
 
-    -1 means no nge was found
-    """
-    results = [-1] * len(heights)  # By default none have nge
+#     # We need to know ahead of time the index of the next nge, and if there is one
 
-    index_stack: list[int] = list()
-    area = 0
+#     indices, area = nge(heights)  # O(n)
 
-    # As long as it is descending, add to stack, when it is not
-    # we will pop the stack until it is descending again.
+#     i = 0
+#     while i < len(heights):
+#         if indices[i] == -1:
+#             # Skip
+#             i += 1
+#             continue
 
-    for i, h in enumerate(heights):
-        # If desc
-        if not index_stack or h < heights[index_stack[-1]]:
-            index_stack.append(i)  # O(1)
-            continue
+#         blocked_area = 0
 
-        # If asc
-        while index_stack and h >= heights[index_stack[-1]]:
-            index = index_stack.pop()
-            results[index] = i  # O(1)
-            area += h - heights[index]
+#         for j in range(i + 1, indices[i]):
+#             # Sum blocking heights
+#             blocked_area += heights[j]
 
-        index_stack.append(i)  # O(1)
+#         # Add the maximum possible area (distance between bars times height of first bar)
+#         # and deduct the blocking bars between them
+#         area += (indices[i] - i - 1) * heights[i] - blocked_area
 
-    return results, area
+#         i = indices[i]
 
-
-def sol(heights: list[int]) -> int:
-    # We can implement a nge - next greatest or equal element, method, that will find the index of the
-    # next biggest element for every index i
-
-    # Then, to simplify rain collection - for example between two big numbers we may have
-    # bars that take the place of a rain water slot, we can deduce that the maximum
-    # rain area between them is at most the distance between them, times the height of the first number
-    # which is always smaller, we then go along the indices between them and deduct the values
-    # of the bars since that is how much space they take
-
-    # We need to know ahead of time the index of the next nge, and if there is one
-
-    indices, area = nge(heights)  # O(n)
-
-    i = 0
-    while i < len(heights):
-        if indices[i] == -1:
-            # Skip
-            i += 1
-            continue
-
-        blocked_area = 0
-
-        for j in range(i + 1, indices[i]):
-            # Sum blocking heights
-            blocked_area += heights[j]
-
-        # Add the maximum possible area (distance between bars times height of first bar)
-        # and deduct the blocking bars between them
-        area += (indices[i] - i - 1) * heights[i] - blocked_area
-
-        i = indices[i]
-
-    return area
+#     return area
 
 
 def sol2(heights: list[int]) -> int:
--- tests/test_42.py
+++ tests/test_42.py
@@ -1,30 +1,14 @@
 import pytest
-from problems.prob_42 import sol, nge, sol2
+from problems.prob_42 import sol2
 
 tests = [
     (([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],), 6),
     (([4, 2, 0, 3, 2, 5],), 9),
     (([4, 2, 3],), 1),
-    # This test fails because i am searching for a number that is 4 or greater,
-    # missing the rain cell that can be created between 4 and 3
-    # if this test were reversed this would of worked because 3 would recognize 4 as its
-    # nge
+    (([4, 2, 1, 2, 4],), 7),
 ]
-
-
-@pytest.mark.parametrize("args, expected", tests)
-def test_0(args, expected):
-    assert sol(*args) == expected
 
 
 @pytest.mark.parametrize("args, expected", tests)
 def test_1(args, expected):
     assert sol2(*args) == expected
-
-
-def test_nge():
-    expected = [4, 3, 3, 4, -1]
-    assert nge([3, 2, 1, 2, 3]) == expected
-
-    expected = [1, 3, 3, 7, 6, 6, 7, -1, 10, 10, -1, -1]
-    assert nge([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == expected

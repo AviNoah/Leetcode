8100616c2e1060f52f4f3425a582d356437cfd43
Avi Noah <AviNoah2019@gmail.com>
2024-10-27 22:34:11 +0200

    solved 1813

--- problems/prob_1813.py
+++ problems/prob_1813.py
@@ -2,19 +2,29 @@
 
 
 def sol(s1: str, s2: str) -> bool:
-    words1 = s1.split()
-    words2 = s2.split()
+    words1 = s1.split()  # Bigger
+    words2 = s2.split()  # Smaller
 
-    # Find the range of non-similarity between left and right
-    # left is where the non-similarity begins, inclusive
-    # right is where the non-similarity ends, exclusive
+    # The key point here is to assign each word from the shorter sentence to either
+    # the longer sentence's prefix, and then to its suffix.
 
-    for left, (w1, w2) in enumerate(zip(words1, words2)):
+    # The prefix and suffix variables mean the amount of words in prefix and suffix respectively.
+    # if the sum of amount of words in prefix and suffix is greater than the shorter sentence or equal -
+    # then they are similar
+    # If greater, that just means prefix and suffix overlap, and either of them can take those words.
+    # only if they are less than we cannot call them similar because there will have to be a word in between
+    # them that is not common.
+
+    prefix = 0
+    for w1, w2 in zip(words1, words2):
         if w1 != w2:
             break
+        prefix += 1
 
-    for right, (w1, w2) in enumerate(zip(reversed(words1), reversed(words2))):
+    suffix = 0
+    for w1, w2 in zip(reversed(words1), reversed(words2)):
         if w1 != w2:
             break
+        suffix += 1
 
-    return
+    return prefix + suffix >= min(len(words1), len(words2))

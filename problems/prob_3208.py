a4bdb6a14ed3fb53812a35dd7586356a896eee27
Avi Noah <AviNoah2019@gmail.com>
2025-03-09 23:40:42 +0200

    3208 1st revis

---
+++ problems/prob_3208.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/alternating-groups-ii/description/?envType=daily-question&envId=2025-03-09
+
+# 0 = red
+# 1 = blue
+
+
+def func(colors: list[int], k: int):
+    # Maybe bit manipulation can enhance performance? we will task with that l8r.
+    # for now a sliding window approach of size K seems to work in theory
+
+    count = 0
+
+    start = 0
+    end = 0
+
+    for i in range(len(colors) + k):
+        if end - start == k - 1:
+            count += 1
+            start += 1
+
+        if colors[i % len(colors)] != colors[(i - 1) % len(colors)]:
+            end += 1
+        else:
+            start = end = i
+
+    return count
---
+++ tests/test_3208.py
@@ -0,0 +1,14 @@
+import pytest
+from problems.prob_3208 import func
+
+
+tests = [
+    (([0, 1, 0, 1, 0], 3), 3),
+    (([0, 1, 0, 0, 1, 0, 1], 6), 2),
+    (([1, 1, 0, 1], 4), 0),
+]
+
+
+@pytest.mark.parametrize("args, expected", tests)
+def test_0(args, expected):
+    assert func(*args) == expected

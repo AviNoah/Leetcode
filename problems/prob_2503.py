32461885ab55e4dc83465f9a22e7ffbdb0322f90
Avi Noah <AviNoah2019@gmail.com>
2025-04-13 18:12:43 +0300

    instread of next_stack, use min_heap to find the next query closest to the threshold

--- problems/prob_2503.py
+++ problems/prob_2503.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/maximum-number-of-points-from-grid-queries/?envType=daily-question&envId=2025-04-13
+import heapq
 
 
 def func(grid: list[list[int]], queries: list[int]) -> list[int]:
@@ -12,38 +13,27 @@ def func(grid: list[list[int]], queries: list[int]) -> list[int]:
 
     QUERIES_DICT: dict[int, int] = {}
 
-    stack = [(0, 0)]
+    # Use a min-heap to always process the lowest-valued cell first
+    heap = [(grid[0][0], 0, 0)]  # Min-heap with (value, row, col)
     count = 0
     visited = set()
+    visited.add((0, 0))
 
     # O(N*M) - count of queries don't really matter
     for query in sorted(queries):
-        next_stack = []
-
-        while stack:
-            row, col = stack.pop()
-
-            if (row, col) in visited:
-                continue
-
-            if query <= grid[row][col]:
-                next_stack.append((row, col))
-                continue
+        # Process all reachable cells with value < query
+        while heap and heap[0][0] < query:
+            val, row, col = heapq.heappop(heap)
 
             count += 1
-            visited.add((row, col))
-
-            if row > 0:
-                stack.append((row - 1, col))
-            if row < ROWS - 1:
-                stack.append((row + 1, col))
-            if col > 0:
-                stack.append((row, col - 1))
-            if col < COLS - 1:
-                stack.append((row, col + 1))
+            # Visit all neighbors
+            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nr, nc = row + dr, col + dc
+                if 0 <= nr < ROWS and 0 <= nc < COLS and (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    heapq.heappush(heap, (grid[nr][nc], nr, nc))
 
         QUERIES_DICT[query] = count
-        stack = next_stack
 
     # O(q)
     return [QUERIES_DICT[query] for query in queries]

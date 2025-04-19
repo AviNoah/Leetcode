db97d58d08236e211886e7328af1c60bbdee9d22
Avi Noah <AviNoah2019@gmail.com>
2025-04-19 22:54:50 +0300

    Added test cases

--- problems/prob_3108.py
+++ problems/prob_3108.py
@@ -7,9 +7,17 @@ def func(n: int, edges: list, query: list):
     # bitwise and for weights - maybe there is a clever trick to optimize calculations? or preprocess?
     # We can travel the same node multiple times, normally this is bad but we do bit-wise and, maybe its good?
     # The puzzle hints at the existence of impossible paths, we need to be able to stop at some point
-    
+
     # The obvious most minimum solution would be to get 0 at some point, since it 'bit-wise and' something else
     # is always 0.
     # The number needed for any number to be 0 when bit-wise and'd is its bit-wise inverse
     # 0110110 XOR 1111111 - we can use a bit-wise XOR with the mask 1*len(input)
-    pass
+
+    # Preprocess or something
+
+    # Iterate over each query
+    results = []
+    for [from_node, to_node] in query:
+        pass
+
+    return results
